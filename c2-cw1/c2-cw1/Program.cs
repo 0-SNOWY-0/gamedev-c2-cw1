911b0ad7bf4c9451d735c02c7cc8a450f747a079
0-SNOWY-0 <73711423+0-SNOWY-0@users.noreply.github.com>
2022-03-05 13:27:44 +0300

    .
    .

---
+++ c2-cw1/c2-cw1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace c2_cw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string heroname = "mario";
+            int age = 15;
+            double herospeed = 2.50;
+            string superpower1 = "fly";
+            string superpower2 = "fast speed";
+            Console.WriteLine("I am " + heroname + " and my age is " + age + " and my speed is " +  herospeed +" and my super power is " +  superpower1 +" and " +  superpower2);
+        }
+    }
+}
